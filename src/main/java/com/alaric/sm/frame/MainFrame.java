ba3eb991e628bde6d82f23b572a839cc80ffa928
wangchengrui <1872164629@qq.com>
2020-11-25 21:11:26 +0800

    学生管理系统

--- src/main/java/com/alaric/sm/frame/MainFrame.java
+++ src/main/java/com/alaric/sm/frame/MainFrame.java
@@ -1,6 +1,11 @@
 package com.alaric.sm.frame;
 
+import com.alaric.sm.entity.Department;
+import com.alaric.sm.factory.ServiceFactory;
+
 import javax.swing.*;
+import java.awt.*;
+import java.util.List;
 
 /**
  * @ClassName MainFrame
@@ -26,4 +31,36 @@ public class MainFrame extends JFrame {
     public static void main (String[] args) {
         new MainFrame();
     }
+
+    private void showDepartments() {
+        //移除原有数据
+        departmentPanel.removeAll();
+        //从service层获取到所有院系西列表
+        List<Department> departmentList = ServiceFactory.getDepartmentServiceInstance().selectAll();
+        //获取总数
+        int len = departmentList.size();
+        //根据院系总数算出行数 （每行放4个）
+        int row = len % 4 == 0 ? len / 4 : len / 4 + 1;
+        //创建一个网络布局，每行4列，指定水平和垂直间距
+        GridLayout gridLayout = new GridLayout(row , 4, 15, 15);
+        departmentPanel.setLayout(gridLayout);
+        for (Department department : departmentList) {
+            //给每个院系对象创建一个面板
+            JPanel depPanel = new JPanel();
+            //设置合适大小
+            depPanel.setPreferredSize(new Dimension(200,200));
+            //将原系名称设置给面板标题
+            depPanel.setBorder(BorderFactory.createTitledBorder(department.getDepartmentName()));
+            //新建一个JLabel标签，用来放置院系logo，并指定大小
+            JLabel logoLabel = new JLabel("<htme><img src='" + department.getLogo() + "'width = 200 height = 200/></html>");
+            //图标标签加入院系内容面板
+            depPanel.add(logoLabel);
+            //院系面板加入主题内容面板
+            departmentPanel.add(depPanel);
+            //刷新主题内容面板
+            departmentPanel.revalidate();
+        }
+    }
+
+
 }
